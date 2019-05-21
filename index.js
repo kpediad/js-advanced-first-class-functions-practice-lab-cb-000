2650d22cb80e19f51ef7a6fd62bc5d12f1006d44
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 16:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,4 @@
 function logDriverNames(drivers) {
   drivers.forEach(function (driver) {console.log(driver.name);});
 }
-function logDriversByHometown(drivers, location) {
-  drivers.forEach(function (driver) {
-    if (driver.hometown === location) {
-      console.log(driver.name);
-    });
-  }
-}
+
